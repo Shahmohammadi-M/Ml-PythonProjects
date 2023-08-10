641f14ba7c26ab8f72ea61fa170e9787296c267c
Shahmohammadi-M <123525022+Shahmohammadi-M@users.noreply.github.com>
2023-08-10 12:56:10 +1000

    Add files via upload

---
+++ Salary.py
@@ -0,0 +1,10 @@
+def hoghoogh(hour,per_hour):
+   if hour>8:
+       return 'error! too much work'
+   else:
+       kole_hoghoogh=hour * per_hour
+       return kole_hoghoogh
+
+print(hoghoogh(13, 2))
+    
+
